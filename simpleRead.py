913f011d369c545ad831dc1405c7aeaa66fa4901
Justine Cocchi <jucocchi@microsoft.com>
2017-08-18 11:46:20 -0700

    update simple ex for readability

--- simpleRead.py
+++ simpleRead.py
@@ -4,6 +4,9 @@ import Adafruit_ADS1x15
 # Create the ADC 12 bit instance
 adc = Adafruit_ADS1x15.ADS1015()
 
+# Max reading value for 12 bit instance
+MAX_READING_VAL = 2047
+
 # Choose a gain depending on what range of voltages you expect
 #  - 2/3 = +/-6.144V
 #  -   1 = +/-4.096V
@@ -13,11 +16,23 @@ adc = Adafruit_ADS1x15.ADS1015()
 #  -  16 = +/-0.256V
 GAIN = 1
 
+# Define channels each sensor is connected to
+HUMIDITY_CHANNEL = 0
+LIGHT_CHANNEL = 1
+
 print('| Humidity | Light |')
+print('--------------------')
 while True:
     values = [0]*2
-    for i in range(2):
-        values[i] = adc.read_adc(i, gain=GAIN)
+
+    # Read from channel 0 for the humidity value
+    values[0] = adc.read_adc(HUMIDITY_CHANNEL, gain=GAIN)
+    # Read from channel 1 for the light value
+    values[1] = adc.read_adc(LIGHT_CHANNEL, gain=GAIN)
+
     print('| {0:>8} | {1:>5} |'.format(*values))
     time.sleep(0.5)
-    
+
+
+def convert_to_percent(value):
+    return 100 - ((value / MAX_READING_VAL) * 100)
